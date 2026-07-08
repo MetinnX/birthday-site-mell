4ba9306d78b656d9bf76be03e6f67bf8e8de5d0d
MetinnX <aditya.utin4@gmail.com>
2026-07-08 12:12:22 +0800

    Update BookScene.jsx

--- src/components/BookScene.jsx
+++ src/components/BookScene.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import BookMessage from './BookMessage';
 import { playBgMusic } from '../utils/music';
 
